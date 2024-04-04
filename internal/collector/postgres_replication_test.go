f063f4ddfb30862c3913d95e52ed32d70dd4eaaf
Mikhail Grigorev <grigorev_mm@magnit.ru>
2024-04-04 13:18:54 +0500

    FIxed replication test

--- internal/collector/postgres_replication_test.go
+++ internal/collector/postgres_replication_test.go
@@ -36,21 +36,21 @@ func Test_parsePostgresReplicationStats(t *testing.T) {
 				Nrows: 1,
 				Ncols: 14,
 				Colnames: []pgproto3.FieldDescription{
-					{Name: []byte("pid")}, {Name: []byte("client_addr")}, {Name: []byte("user")}, {Name: []byte("application_name")}, {Name: []byte("state")},
+					{Name: []byte("pid")}, {Name: []byte("client_addr")},{Name: []byte("client_port")}, {Name: []byte("user")}, {Name: []byte("application_name")}, {Name: []byte("state")},
 					{Name: []byte("pending_lag_bytes")}, {Name: []byte("write_lag_bytes")}, {Name: []byte("flush_lag_bytes")},
 					{Name: []byte("replay_lag_bytes")}, {Name: []byte("total_lag_bytes")}, {Name: []byte("write_lag_seconds")},
 					{Name: []byte("flush_lag_seconds")}, {Name: []byte("replay_lag_seconds")}, {Name: []byte("total_lag_seconds")},
 				},
 				Rows: [][]sql.NullString{
 					{
-						{String: "123456", Valid: true}, {String: "127.0.0.1", Valid: true}, {String: "testuser", Valid: true}, {String: "testapp", Valid: true},
+						{String: "123456", Valid: true}, {String: "127.0.0.1", Valid: true}, {String: "51658", Valid: true}, {String: "testuser", Valid: true}, {String: "testapp", Valid: true},
 						{String: "teststate", Valid: true},
 						{String: "100", Valid: true}, {String: "200", Valid: true}, {String: "300", Valid: true}, {String: "400", Valid: true},
 						{String: "500", Valid: true}, {String: "600", Valid: true}, {String: "700", Valid: true}, {String: "800", Valid: true}, {String: "2100", Valid: true},
 					},
 					{
 						// pg_receivewals and pg_basebackups don't have replay lag.
-						{String: "101010", Valid: true}, {String: "127.0.0.1", Valid: true}, {String: "testuser", Valid: true}, {String: "pg_receivewal", Valid: true},
+						{String: "101010", Valid: true}, {String: "127.0.0.1", Valid: true}, {String: "52441", Valid: true}, {String: "testuser", Valid: true}, {String: "pg_receivewal", Valid: true},
 						{String: "teststate", Valid: true},
 						{String: "4257", Valid: true}, {String: "8452", Valid: true}, {String: "5785", Valid: true}, {String: "", Valid: false},
 						{String: "", Valid: false}, {String: "2458", Valid: true}, {String: "7871", Valid: true}, {String: "6896", Valid: true}, {String: "17225", Valid: true},
@@ -59,14 +59,14 @@ func Test_parsePostgresReplicationStats(t *testing.T) {
 			},
 			want: map[string]postgresReplicationStat{
 				"123456": {
-					pid: "123456", clientaddr: "127.0.0.1", user: "testuser", applicationName: "testapp", state: "teststate",
+					pid: "123456", clientaddr: "127.0.0.1", clientport: "51658", user: "testuser", applicationName: "testapp", state: "teststate",
 					values: map[string]float64{
 						"pending_lag_bytes": 100, "write_lag_bytes": 200, "flush_lag_bytes": 300, "replay_lag_bytes": 400, "total_lag_bytes": 500,
 						"write_lag_seconds": 600, "flush_lag_seconds": 700, "replay_lag_seconds": 800, "total_lag_seconds": 2100,
 					},
 				},
 				"101010": {
-					pid: "101010", clientaddr: "127.0.0.1", user: "testuser", applicationName: "pg_receivewal", state: "teststate",
+					pid: "101010", clientaddr: "127.0.0.1", clientport: "52441", user: "testuser", applicationName: "pg_receivewal", state: "teststate",
 					values: map[string]float64{
 						"pending_lag_bytes": 4257, "write_lag_bytes": 8452, "flush_lag_bytes": 5785,
 						"write_lag_seconds": 2458, "flush_lag_seconds": 7871, "replay_lag_seconds": 6896, "total_lag_seconds": 17225,
@@ -78,7 +78,7 @@ func Test_parsePostgresReplicationStats(t *testing.T) {
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
-			got := parsePostgresReplicationStats(tc.res, []string{"client_addr", "user", "application_name", "state", "type"})
+			got := parsePostgresReplicationStats(tc.res, []string{"client_addr", "client_port", "user", "application_name", "state", "type"})
 			assert.EqualValues(t, tc.want, got)
 		})
 	}
