6e76ef16432b319d43a4aaf148484db20aa8dc68
Mikhail Grigorev <grigorev_mm@magnit.ru>
2024-09-09 20:15:36 +0500

    Fixed bgwriter test

--- internal/collector/postgres_bgwriter_test.go
+++ internal/collector/postgres_bgwriter_test.go
@@ -39,13 +39,14 @@ func Test_parsePostgresBgwriterStats(t *testing.T) {
 			name: "normal output",
 			res: &model.PGResult{
 				Nrows: 1,
-				Ncols: 11,
+				Ncols: 15,
 				Colnames: []pgproto3.FieldDescription{
 					{Name: []byte("checkpoints_timed")}, {Name: []byte("checkpoints_req")},
 					{Name: []byte("checkpoint_write_time")}, {Name: []byte("checkpoint_sync_time")},
 					{Name: []byte("buffers_checkpoint")}, {Name: []byte("buffers_clean")}, {Name: []byte("maxwritten_clean")},
 					{Name: []byte("buffers_backend")}, {Name: []byte("buffers_backend_fsync")}, {Name: []byte("buffers_alloc")},
-					{Name: []byte("bgwr_stats_age_seconds")},
+					{Name: []byte("bgwr_stats_age_seconds")}, {Name: []byte("ckpt_stats_age_seconds")}, {Name: []byte("restartpoints_timed")},
+					{Name: []byte("restartpoints_req")}, {Name: []byte("restartpoints_done")},
 				},
 				Rows: [][]sql.NullString{
 					{
@@ -53,13 +54,15 @@ func Test_parsePostgresBgwriterStats(t *testing.T) {
 						{String: "548425", Valid: true}, {String: "5425", Valid: true},
 						{String: "5482", Valid: true}, {String: "7584", Valid: true}, {String: "452", Valid: true},
 						{String: "6895", Valid: true}, {String: "2", Valid: true}, {String: "48752", Valid: true},
-						{String: "5488", Valid: true},
+						{String: "5488", Valid: true}, {String: "54388", Valid: true}, {String: "47352", Valid: true},
+						{String: "5288", Valid: true}, {String: "1438", Valid: true},
 					},
 				},
 			},
 			want: postgresBgwriterStat{
 				ckptTimed: 55, ckptReq: 17, ckptWriteTime: 548425, ckptSyncTime: 5425, ckptBuffers: 5482, bgwrBuffers: 7584, bgwrMaxWritten: 452,
-				backendBuffers: 6895, backendFsync: 2, backendAllocated: 48752, bgwrStatsAgeSeconds: 5488,
+				backendBuffers: 6895, backendFsync: 2, backendAllocated: 48752, bgwrStatsAgeSeconds: 5488, ckptStatsAgeSeconds: 54388, ckptRestartpointsTimed: 47352,
+				ckptRestartpointsReq: 5288, ckptRestartpointsDone: 1438,
 			},
 		},
 	}
