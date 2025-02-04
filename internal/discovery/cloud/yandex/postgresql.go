2158daea2e18fb36e28d64d1d7b5152211242d00
Dmitry V. Bulashev <d.bulashev@gmail.com>
2025-02-04 12:11:01 +0500

    Fix: Iterate through all clusters if the number of clusters is greater than 100

--- internal/discovery/cloud/yandex/postgresql.go
+++ internal/discovery/cloud/yandex/postgresql.go
@@ -44,88 +44,94 @@ func (sdk *SDK) GetPostgreSQLClusters(ctx context.Context, folderID string, filt
 		return nil, err
 	}
 
+	var clusters []Cluster
 	var req postgresql.ListClustersRequest
 	req.FolderId = folderID
-	resp, err := yandexSdk.MDB().PostgreSQL().Cluster().List(ctx, &req)
-	if err != nil {
-		log.Errorf("[Yandex.Cloud SD] Failed to get cluster list, error: %v", err)
-		return nil, err
-	}
-	var clusters []Cluster
-	for _, cluster := range resp.Clusters {
-		if !(cluster.Status == postgresql.Cluster_RUNNING || cluster.Status == postgresql.Cluster_UPDATING) {
-			log.Debugf("[Yandex.Cloud SD] Cluster '%s' is not running, skipped.", cluster.Name)
-			continue
-		}
-		log.Debugf("[Yandex.Cloud SD] Found cluster '%s'", cluster.Name)
-		matched := make([]int, 0)
-		for c, filterCluster := range filter {
-			if !filterCluster.MatchName(cluster.Name) {
-				log.Debugf("[Yandex.Cloud SD] Cluster '%s' not matched in filters, skipped.", cluster.Name)
-				continue
-			}
-			log.Debugf("[Yandex.Cloud SD] Cluster '%s' matched in filters.", cluster.Name)
-			matched = append(matched, c)
-		}
-		if len(matched) == 0 {
-			continue
-		}
-		var hosts []Host
-		var databases []Database
-		hostsIterator := yandexSdk.MDB().PostgreSQL().Cluster().ClusterHostsIterator(ctx,
-			&postgresql.ListClusterHostsRequest{ClusterId: cluster.Id})
-		for hostsIterator.Next() {
-			host := hostsIterator.Value()
-			if host.Health != postgresql.Host_ALIVE {
-				continue
-			}
-			hosts = append(hosts, Host{
-				Name:   host.Name,
-				ZoneID: host.ZoneId,
-				Role:   host.Role,
-				Health: host.Health,
-			})
-		}
-		log.Debugf("[Yandex.Cloud SD] In cluster '%s' found '%d' hosts.", cluster.Name, len(hosts))
-
-		dbResp, err := yandexSdk.MDB().PostgreSQL().Database().List(ctx,
-			&postgresql.ListDatabasesRequest{ClusterId: cluster.Id})
+	for {
+		resp, err := yandexSdk.MDB().PostgreSQL().Cluster().List(ctx, &req)
 		if err != nil {
+			log.Errorf("[Yandex.Cloud SD] Failed to get cluster list, error: %v", err)
 			return nil, err
 		}
-		for _, database := range dbResp.Databases {
-			skip := true
-			for _, f := range matched {
-				if !filter[f].MatchDb(database.Name) {
+		for _, cluster := range resp.Clusters {
+			if !(cluster.Status == postgresql.Cluster_RUNNING || cluster.Status == postgresql.Cluster_UPDATING) {
+				log.Debugf("[Yandex.Cloud SD] Cluster '%s' is not running, skipped.", cluster.Name)
+				continue
+			}
+			log.Debugf("[Yandex.Cloud SD] Found cluster '%s'", cluster.Name)
+			matched := make([]int, 0)
+			for c, filterCluster := range filter {
+				if !filterCluster.MatchName(cluster.Name) {
+					log.Debugf("[Yandex.Cloud SD] Cluster '%s' not matched in filters, skipped.", cluster.Name)
 					continue
 				}
-				skip = false
-				break
+				log.Debugf("[Yandex.Cloud SD] Cluster '%s' matched in filters.", cluster.Name)
+				matched = append(matched, c)
 			}
-			if !skip {
-				databases = append(databases, Database{
-					Name:  database.Name,
-					Owner: database.Owner,
+			if len(matched) == 0 {
+				continue
+			}
+			var hosts []Host
+			var databases []Database
+			hostsIterator := yandexSdk.MDB().PostgreSQL().Cluster().ClusterHostsIterator(ctx,
+				&postgresql.ListClusterHostsRequest{ClusterId: cluster.Id})
+			for hostsIterator.Next() {
+				host := hostsIterator.Value()
+				if host.Health != postgresql.Host_ALIVE {
+					continue
+				}
+				hosts = append(hosts, Host{
+					Name:   host.Name,
+					ZoneID: host.ZoneId,
+					Role:   host.Role,
+					Health: host.Health,
 				})
 			}
+			log.Debugf("[Yandex.Cloud SD] In cluster '%s' found '%d' hosts.", cluster.Name, len(hosts))
+
+			dbResp, err := yandexSdk.MDB().PostgreSQL().Database().List(ctx,
+				&postgresql.ListDatabasesRequest{ClusterId: cluster.Id})
+			if err != nil {
+				return nil, err
+			}
+			for _, database := range dbResp.Databases {
+				skip := true
+				for _, f := range matched {
+					if !filter[f].MatchDb(database.Name) {
+						continue
+					}
+					skip = false
+					break
+				}
+				if !skip {
+					databases = append(databases, Database{
+						Name:  database.Name,
+						Owner: database.Owner,
+					})
+				}
+			}
+			if len(databases) == 0 {
+				log.Debugf("[Yandex.Cloud SD] No databases found in cluster '%s'.", cluster.Name)
+				continue
+			}
+			log.Debugf("[Yandex.Cloud SD] In cluster '%s' found '%d' databases.", cluster.Name, len(databases))
+			clusters = append(clusters, Cluster{
+				ID:               cluster.Id,
+				Name:             cluster.Name,
+				FolderID:         cluster.FolderId,
+				Health:           cluster.Health,
+				Status:           cluster.Status,
+				ResourcePresetID: cluster.Config.Resources.ResourcePresetId,
+				DiskTypeID:       cluster.Config.Resources.DiskTypeId,
+				DiskSize:         cluster.Config.Resources.DiskSize,
+				Hosts:            hosts,
+				Databases:        databases,
+			})
 		}
-		if len(databases) == 0 {
-			log.Debugf("[Yandex.Cloud SD] No databases found in cluster '%s'.", cluster.Name)
-			continue
+		if resp.NextPageToken == "" {
+			break
 		}
-		log.Debugf("[Yandex.Cloud SD] In cluster '%s' found '%d' databases.", cluster.Name, len(databases))
-		clusters = append(clusters, Cluster{
-			ID:               cluster.Id,
-			Name:             cluster.Name,
-			FolderID:         cluster.FolderId,
-			Health:           cluster.Health,
-			Status:           cluster.Status,
-			ResourcePresetID: cluster.Config.Resources.ResourcePresetId,
-			DiskTypeID:       cluster.Config.Resources.DiskTypeId,
-			DiskSize:         cluster.Config.Resources.DiskSize,
-			Hosts:            hosts,
-			Databases:        databases,
-		})
+		req.PageToken = resp.NextPageToken
 	}
 	return clusters, nil
 }
