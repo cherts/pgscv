4896565c13d4ad1bd6c6c4497807ad0b6363dccd
Dmitry V. Bulashev <d.bulashev@gmail.com>
2025-02-06 18:11:41 +0500

    fixup

--- cmd/pgscv.go
+++ cmd/pgscv.go
@@ -4,8 +4,8 @@ package main
 import (
 	"context"
 	"fmt"
-	"github.com/cherts/pgscv/discovery/instantiate"
-	sd_log "github.com/cherts/pgscv/discovery/log"
+	"github.com/cherts/pgscv/discovery/factory"
+	sdlog "github.com/cherts/pgscv/discovery/log"
 	"os"
 	"os/signal"
 	"syscall"
@@ -28,10 +28,10 @@ func main() {
 	kingpin.Parse()
 	log.SetLevel(*logLevel)
 	log.SetApplication(appName)
-	sd_log.Logger.Debug = log.Debug
-	sd_log.Logger.Errorf = log.Errorf
-	sd_log.Logger.Infof = log.Infof
-	sd_log.Logger.Debugf = log.Debugf
+	sdlog.Logger.Debug = log.Debug
+	sdlog.Logger.Errorf = log.Errorf
+	sdlog.Logger.Infof = log.Infof
+	sdlog.Logger.Debugf = log.Debugf
 	if *showVersion {
 		fmt.Printf("%s %s %s-%s\n", appName, gitTag, gitCommit, gitBranch)
 		os.Exit(0)
@@ -46,9 +46,9 @@ func main() {
 	}
 
 	if config.DiscoveryConfig != nil {
-		config.DiscoveryServices, err = instantiate.Instantiate(*config.DiscoveryConfig)
+		config.DiscoveryServices, err = factory.Instantiate(*config.DiscoveryConfig)
 		if err != nil {
-			log.Errorln("instantiate discovery failed: ", err)
+			log.Errorln("factory discovery failed: ", err)
 			os.Exit(1)
 		}
 	}
