bec25673c63f75b5fd7fc7d0eb709565d22b9f7a
Mikhail Grigorev <grigorev_mm@magnit.ru>
2025-02-28 14:16:40 +0500

    Fixed extend_bytes

--- internal/collector/postgres_stat_io.go
+++ internal/collector/postgres_stat_io.go
@@ -23,7 +23,7 @@ const (
 
 	postgresStatIoQueryLatest = "SELECT backend_type, object, context, COALESCE(reads, 0) AS reads, COALESCE(read_time, 0) AS read_time, " +
 		"COALESCE(writes, 0) AS writes, COALESCE(write_time, 0) AS write_time, COALESCE(writebacks, 0) AS writebacks, " +
-		"COALESCE(writeback_time, 0) AS writeback_time, coalesce(extends, 0) AS extends, COALESCE(extend_time, 0) AS extend_time, " +
+		"COALESCE(writeback_time, 0) AS writeback_time, COALESCE(extends, 0) AS extends, COALESCE(extend_time, 0) AS extend_time, " +
 		"COALESCE(hits, 0) AS hits, COALESCE(evictions, 0) AS evictions, COALESCE(reuses, 0) AS reuses, " +
 		"COALESCE(fsyncs, 0) AS fsyncs, COALESCE(fsync_time, 0) AS fsync_time, " +
 		"COALESCE(read_bytes, 0) AS read_bytes, COALESCE(write_bytes, 0) AS write_bytes, COALESCE(extend_bytes, 0) AS extend_bytes " +
@@ -300,7 +300,7 @@ func parsePostgresStatIO(r *model.PGResult, labelNames []string) map[string]post
 				s.ReadBytes = v
 			case "write_bytes":
 				s.WriteBytes = v
-			case "extends_bytes":
+			case "extend_bytes":
 				s.ExtendsBytes = v
 			default:
 				continue
