54edf23c2c2b7695a04ed1f7cee43973dde65b21
Mikhail Grigorev <grigorev_mm@magnit.ru>
2025-03-12 17:40:18 +0500

    Fixed typo

--- internal/collector/postgres_storage.go
+++ internal/collector/postgres_storage.go
@@ -16,8 +16,8 @@ import (
 )
 
 const (
-	postgresTempFilesInflightQuery = "SELECT ts.spcname AS tablespace, coalesce(count(size), 0) AS files_total, coalesce(sum(size), 0) AS bytes_total, " +
-		"coalesce(extract(epoch from clock_timestamp() - min(modification)), 0) AS max_age_seconds " +
+	postgresTempFilesInflightQuery = "SELECT ts.spcname AS tablespace, COALESCE(COUNT(size), 0) AS files_total, COALESCE(sum(size), 0) AS bytes_total, " +
+		"COALESCE(EXTRACT(EPOCH FROM clock_timestamp() - min(modification)), 0) AS max_age_seconds " +
 		"FROM pg_tablespace ts LEFT JOIN (SELECT spcname,(pg_ls_tmpdir(oid)).* FROM pg_tablespace WHERE spcname != 'pg_global') ls ON ls.spcname = ts.spcname " +
 		"WHERE ts.spcname != 'pg_global' GROUP BY ts.spcname"
 )
