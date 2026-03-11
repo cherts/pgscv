a32cc81ed0d8a498a1d121040b69f73eeac6008b
Mikhail Grigorev <grigorev_mm@magnit.ru>
2026-03-11 19:46:20 +0500

    Fixed

--- deploy/demo-lab/postgres/pg_archive_data/archive_wal.sh
+++ deploy/demo-lab/postgres/pg_archive_data/archive_wal.sh
@@ -10,4 +10,4 @@ if [ ! -d "/data/postgres_archive/${PG_HOSTNAME}" ]; then
   mkdir -p "/data/postgres_archive/${PG_HOSTNAME}" 2>/dev/null
 fi
 
-gzip < $1 > /data/postgres_archive/${PG_HOSTNAME}/$2.gz
+gzip < /data/postgres/$1 > /data/postgres_archive/${PG_HOSTNAME}/$2.gz
