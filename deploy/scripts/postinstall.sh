a8013e9456e8c96c9e1595ea74c512ded01e6653
Mikhail Grigorev <grigorev_mm@magnit.ru>
2026-07-17 01:38:53 +0500

    Update package scripts

--- deploy/scripts/postinstall.sh
+++ deploy/scripts/postinstall.sh
@@ -1,10 +1,19 @@
 #!/bin/sh
 
+# Determine OS platform
+# shellcheck source=/dev/null
+. /etc/os-release
+
 restart_pgscv_if_required() {
     if service pgscv status >/dev/null 2>&1; then
         echo "PostInstall: Restarting pgscv service"
         service pgscv restart >/dev/null 2>&1 || true
     fi
+}
+
+add_pgscv_openrc() {
+    echo "PostInstall: Adding pgscv to openrc"
+    rc-update add pgscv default >/dev/null 2>&1 || true
 }
 
 set_config_owner() {
@@ -27,10 +36,29 @@ summary() {
     echo "----------------------------------------------------------------------"
 }
 
-{
-    set_config_owner
-    restart_pgscv_if_required
-    summary
+summary_alpine() {
+    echo "----------------------------------------------------------------------"
+    echo " pgSCV package has been successfully installed."
+    echo ""
+    echo " Please follow the next steps to start the software:"
+    echo "    rc-service pgscv start"
+    echo ""
+    echo " Configuration settings can be adjusted here:"
+    echo "    /etc/pgscv.yaml"
+    echo ""
+    echo "----------------------------------------------------------------------"
 }
+
+set_config_owner
+restart_pgscv_if_required
+case "$ID" in
+    alpine)
+        add_pgscv_openrc
+        summary_alpine
+        ;;
+    *)
+        summary
+        ;;
+esac
 
 exit 0
